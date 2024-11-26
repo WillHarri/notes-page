11bb0ded09846a55133a8a419a723601ae12e9b3
william.harrison <william.harrison1@engineering.digital.dwp.gov.uk>
2024-11-26 16:09:14 +0000

    works

--- htmljavascript.js
+++ htmljavascript.js
@@ -9,6 +9,7 @@ urlGlobe = "http://localhost:3000/notes/";
 const postNotes = document.getElementById("postNotes");
 const messageBox = document.getElementById("messageBox");
 
+
 async function PostNotesFunc() {
   console.log("PostNotesFunc");
   let id = "";
@@ -39,6 +40,38 @@ async function PostNotesFunc() {
 // -------------------------------------------------------------------
 // GET
 // -------------------------------------------------------------------
+
+const idSelector = document.getElementById("idSelector");
+const dateBox = document.getElementById("dateBox");
+const noteBox = document.getElementById("noteBox");
+
+idSelector.addEventListener("change", () => {
+  const id = idSelector.value;
+  updateDisplay(id);
+});
+
+
+async function updateDisplay(id) {
+
+  try {
+    const response = await fetch(urlGlobe + id, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+      },
+    });
+
+    const jsonData = await response.json();
+    noteBox.textContent = jsonData.notes;
+    dateBox.textContent = jsonData.date;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
 
 // -------------------------------------------------------------------
 // DEL
